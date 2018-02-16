19dbba5488d37f9c5ed6253a8f084da953f96b50
Larry Cessna <larry.cessna@outlook.com>
2018-02-15 19:22:06 -0800

    Added comments to AboutController.js

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -5,7 +5,7 @@ var app = app || {};
   const aboutController = {};
 
   // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
-  // (put your response in a comment here)
+  // 1. This function shows the about page. 2. The function is being called by page.js. 3. Yes, it calls requestRepos from repo.js.
   aboutController.index = () => {
     $('#about').show().siblings().hide();
     app.repos.requestRepos(app.repoView.index);
